4c2d332b507810f93ac117ba0b1efe0d314521f7
Keaten Jones <keatenjones16@gmail.com>
2020-01-30 17:46:02 -0700

    Implemented Message cache and cool easter egg.

--- src/cogs/maincog.py
+++ src/cogs/maincog.py
@@ -8,6 +8,10 @@ class MainCog(commands.Cog):
     def __init__(self, bot ):
         self.bot = bot
         self.slurrs = ["nigger", "coon", "nigga", "white trash", "yellow skin", "wetback", "spick", "chink", "beaner", "izzy"]
+        self.message_cache = []
+        self.message_count = 0
+        self.same_message_count = 0
+
 
     async def on_ready(self):
         print("ScrapBot is Ready.")
@@ -16,16 +20,27 @@ class MainCog(commands.Cog):
         await member.send("Welcome to the server " + str(member) + " !")
     
     async def on_message(self, message):
-        slurs = False
-        slurs = await checkSlurrs(message, self.slurrs)
-        if slurs:
-            await message.delete()
-            await message.channel.send(str(message.author) +" is a racist!")
-        if message.author == bot.user:
-            return
-        if message.content.lower() in slurrs:
-            await message.delete()
-            await message.channel.send(str(message.author) +" is a racist!")
+        if(self.message_count > 20 ):
+            self.message_count = 0 
+            self.message_cache[self.message_count] = message
+        else:
+            self.message_cache[self.message_count] = message
+            self.message_count += 1
+        
+        checkword = self.message_cache[0]
+        sameword = 0
+        for i in range(len(self.message_cache)):
+            if(checkword.content == self.message_cache[i].content):
+                sameword += 1
+            else:
+                sameword = 0
+                checkword = self.message_cache[i]
+
+            if(sameword == 3):
+                await message.channel.send(checkword.content)
+        if(sameword == 2):
+            await message.channel.send("Jinx!")
+
 
 
 ############################# Setup Bot #####################################
@@ -33,12 +48,4 @@ def setup(bot):
     bot.add_cog(MainCog(bot))
 
 ############################ Helper Functions ###############################
-async def checkSlurrs(message, slurrs):
-    lower_message = message.content.lower()
-    split = lower_message.split()
-    for i in range(len(slurrs)):
-        for j in range(len(split)):
-            if slurrs[i] == split[j]:
-                return True
-    return
 
